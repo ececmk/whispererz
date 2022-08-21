443d95c25acbc525888e86bb8a48e4ddba0f0cf8
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-08-21 14:21:28 +0200

    Initialize project

---
+++ app.js
@@ -0,0 +1,31 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv/config");
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require("hbs");
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// default value for title local
+const capitalized = require("./utils/capitalized");
+const projectName = "Whispererz";
+
+app.locals.appTitle = `${capitalized(projectName)}`;
+
+// 👇 Start handling routes here
+const index = require("./routes/index.routes")
+app.use("/", index);
+
+module.exports = app;
