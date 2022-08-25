367ba20ef7e145e2c783c786ec31c7c92b3f5a37
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2022-08-25 11:47:34 +0200

    add css to profile and share-secret

--- models/User.model.js
+++ models/User.model.js
@@ -13,9 +13,9 @@ const userSchema = new Schema(
       required: [true, 'Password is required.'],
       trim: true,
     },
-    /*credit: {
+    credit: {
       type: Number,
-    },*/
+    },
   },
   {
     timestamps: true,
